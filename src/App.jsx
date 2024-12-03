d63bc345bc22eab61c5392aaa8bedcd2f2e604e8
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-12-03 12:22:16 +0200

    feature-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/Navbar'
+import Features from './components/Features'
 
 const App = () => {
   return (
     <main className='releative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
       <Hero />
       <About />
+      <Features/>
     </main>
   )
 }
